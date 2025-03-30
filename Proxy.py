fad8ff77088cb7146673c53f320b1298e7d727d9
PUNCH0519 <liuweian0519@gmail.com>
2025-03-30 22:41:55 +1030

    ProxyServer finds a cache hit, Send back response to client

--- Proxy.py
+++ Proxy.py
@@ -118,6 +118,8 @@ while True:
     # ProxyServer finds a cache hit
     # Send back response to client 
     # ~~~~ INSERT CODE ~~~~
+    for DATALine in cacheData:
+      clientSocket.send(DATALine.encode())
     # ~~~~ END CODE INSERT ~~~~
     cacheFile.close()
     print ('Sent to the client:')
